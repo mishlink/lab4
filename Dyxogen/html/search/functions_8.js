a6a17f2dbe7b6321e37592af818743efe277bda0
Misha Sterkhov <mishlink@yandex.ru>
2023-03-09 11:13:20 +0300

    Dox

--- Dyxogen/html/search/functions_8.js
+++ Dyxogen/html/search/functions_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['operator_3c_3c_0',['operator&lt;&lt;',['../namespace_habitations.html#aae2268cc6a10b7b2cb9d38688fa2e0c1',1,'Habitations::operator&lt;&lt;(std::ostream &amp;os, const Room &amp;r)'],['../namespace_habitations.html#ae5c64aafc02cd068a4c7cf76d1cf70eb',1,'Habitations::operator&lt;&lt;(std::ostream &amp;os, const Habitation &amp;h)'],['../namespace_habitations.html#a9951af29941e03067a8de2bc68595208',1,'Habitations::operator&lt;&lt;(std::ostream &amp;os, const Building &amp;b)'],['../namespace_prog4.html#a3caf7f166f13ae5a2aa10453629df29b',1,'Prog4::operator&lt;&lt;()']]],
-  ['operator_3d_1',['operator=',['../class_prog4_1_1_assoc.html#a4a8c142b60b082a649f4df08fb6d03e9',1,'Prog4::Assoc::operator=()'],['../class_prog4_1_1_table.html#a28a2fa9c9f95a0ba9e9561b6fe3aea8c',1,'Prog4::Table::operator=()']]]
+  ['operator_3c_0',['operator&lt;',['../struct_habitations_1_1_adress.html#aae3b054c7ef4227a3520f9342dc23662',1,'Habitations::Adress']]],
+  ['operator_3c_3c_1',['operator&lt;&lt;',['../namespace_habitations.html#aae2268cc6a10b7b2cb9d38688fa2e0c1',1,'Habitations::operator&lt;&lt;(std::ostream &amp;os, const Room &amp;r)'],['../namespace_habitations.html#ae5c64aafc02cd068a4c7cf76d1cf70eb',1,'Habitations::operator&lt;&lt;(std::ostream &amp;os, const Habitation &amp;h)'],['../namespace_habitations.html#a9951af29941e03067a8de2bc68595208',1,'Habitations::operator&lt;&lt;(std::ostream &amp;os, const Building &amp;b)'],['../namespace_prog4.html#a3caf7f166f13ae5a2aa10453629df29b',1,'Prog4::operator&lt;&lt;()']]],
+  ['operator_3d_2',['operator=',['../class_prog4_1_1_assoc.html#a4a8c142b60b082a649f4df08fb6d03e9',1,'Prog4::Assoc::operator=()'],['../class_prog4_1_1_table.html#a28a2fa9c9f95a0ba9e9561b6fe3aea8c',1,'Prog4::Table::operator=()']]]
 ];
--- Dyxogen/html/search/related_0.js
+++ Dyxogen/html/search/related_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['operator_3c_3c_0',['operator&lt;&lt;',['../class_habitations_1_1_room.html#abef980d48029b5948af60bcf6f2eac5b',1,'Habitations::Room::operator&lt;&lt;()'],['../class_habitations_1_1_habitation.html#a823a4945ef666631a22102d14e42df7e',1,'Habitations::Habitation::operator&lt;&lt;()'],['../class_habitations_1_1_building.html#a3f70f2cf1f80abee09c20ed16f1d405b',1,'Habitations::Building::operator&lt;&lt;()'],['../class_prog4_1_1_assoc.html#aa617359904a4bf706592a6a9fd554021',1,'Prog4::Assoc::operator&lt;&lt;()'],['../class_prog4_1_1_table.html#aa617359904a4bf706592a6a9fd554021',1,'Prog4::Table::operator&lt;&lt;()']]]
+  ['operator_3c_3c_0',['operator&lt;&lt;',['../class_habitations_1_1_room.html#abef980d48029b5948af60bcf6f2eac5b',1,'Habitations::Room::operator&lt;&lt;()'],['../class_habitations_1_1_habitation.html#a823a4945ef666631a22102d14e42df7e',1,'Habitations::Habitation::operator&lt;&lt;()'],['../class_habitations_1_1_building.html#a3f70f2cf1f80abee09c20ed16f1d405b',1,'Habitations::Building::operator&lt;&lt;()'],['../class_prog4_1_1_table.html#aa617359904a4bf706592a6a9fd554021',1,'Prog4::Table::operator&lt;&lt;()']]]
 ];
